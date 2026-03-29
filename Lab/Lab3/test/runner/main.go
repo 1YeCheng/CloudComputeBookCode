a78e84c5cd0f0215520bf4d6778c414e944b35db
slightwo <slightwo>
2026-03-29 15:45:18 +0800

    lab3 complete

--- Lab/Lab3/test/runner/main.go
+++ Lab/Lab3/test/runner/main.go
@@ -16,7 +16,7 @@ import (
 var ports = []int{9310, 9311, 9312, 9313}
 
 func main() {
-	target := "complete"
+	target := "student"
 	args := os.Args[1:]
 	if len(args) > 0 && (args[0] == "student" || args[0] == "complete") {
 		target = args[0]
